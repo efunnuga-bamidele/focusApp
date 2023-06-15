adf41e95ad82a8e3f28449b708de4a88fc0e7f62
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-06-15 02:19:46 +0100

    feat:Name Change

--- android/app/src/main/java/com/bjtmtechnologies/focusapp/MainApplication.java
+++ android/app/src/main/java/com/bjtmtechnologies/focusapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.focusapp;
+package com.bjtmtechnologies.focusapp;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- android/app/src/release/java/com/bjtmtechnologies/focusapp/ReactNativeFlipper.java
+++ android/app/src/release/java/com/bjtmtechnologies/focusapp/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.focusapp;
+package com.bjtmtechnologies.focusapp;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
